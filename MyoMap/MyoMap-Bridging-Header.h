e7221b9a9d52819deb6f3a9a90f26b568c2d6d5b
Peter Chen <peter@weme.sh>
2016-03-04 21:42:46 -0500

    First commit

---
+++ MyoMap/MyoMap-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  MyoMap-Bridging-Header.h
+//  MyoMap
+//
+//  Created by Peter Chen on 2016-03-04.
+//  Copyright © 2016 Peter Chen. All rights reserved.
+//
+
+#ifndef MyoMap_Bridging_Header_h
+
+#import <MyoKit/MyoKit.h>
+
+#define MyoMap_Bridging_Header_h
+
+
+#endif /* MyoMap_Bridging_Header_h */
